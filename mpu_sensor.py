a22d0a840fff4c54d9c17b49d473ae2facbfc37a
M Ilham Nofriansyah <126463961+noperi11@users.noreply.github.com>
2025-08-10 16:41:08 +0700

    Update mpu_sensor.py

--- mpu_sensor.py
+++ mpu_sensor.py
@@ -1,5 +1,6 @@
 from gpiozero import Buzzer
 from mpu6050 import mpu6050
+import math
 
 class HeadTilt:
     def __init__(self, threshold_deg=6):
@@ -8,9 +9,13 @@ class HeadTilt:
 
     def get_pitch(self):
         data = self.mpu.get_accel_data()
-        pitch = data['y']
+        ax = data['x']
+        ay = data['y']
+        az = data['z']
+        
+        # Rumus konversi data akselerometer → sudut pitch (derajat)
+        pitch = math.atan2(ay, math.sqrt(ax**2 + az**2)) * (180 / math.pi)
         return pitch
 
     def is_tilted(self):
         return abs(self.get_pitch()) > self.threshold
-
